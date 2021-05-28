020f65dae530a0ab3aace8d0be7afda31790eead
jl7719 <jl7719@ic.ac.uk>
2021-05-28 17:34:42 +0900

    Compute rover-ball distance and image centre-ball centre pixel distance
    these data can be used to find the actual position of the balls on esp32

--- DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
+++ DE10_LITE_D8M_VIP_16/ip/EEE_IMGPROC/EEE_IMGPROC.v
@@ -80,14 +80,16 @@ assign delta = cmax-cmin;
 assign saturation = (cmax == 0) ? 0 : delta/cmax;
 assign hue = (delta == 0) ? 0 : (cmax == red) ? (green - blue)*60/saturation 
 							  : (cmax == green) ? 120 + (blue - red)*60/saturation
-		   					  : (cmax == blue) ? 240 + (red - green)*60/saturation;
+		   					  : 240 + (red - green)*60/saturation;
 assign value = cmax;
 
+wire brightness_filter;
+assign brightness_filter = value >= 60 & value <= 180;
 
 // Detect red, green, blue, yellow, pink and white areas
 wire red_detect, green_detect, blue_detect, white_detect, yellow_detect, pink_detect;
 assign red_detect = red[7] & ~green[7] & ~blue[7];
-assign green_detect = ~red[7] & green <= 8'h99 & green >= 8'h33 & blue <= 8'hf0 & blue >= 8'h60;
+assign green_detect = ~red[7] & green <= 8'h99 & green >= 8'h66 & blue <= 8'hd0 & blue >= 8'h40;
 assign blue_detect = ~red[7] & green <= 8'h66 & green >= 8'h33 & blue <= 8'hcc & blue >= 8'h66;
 assign yellow_detect = red[7] & green[7] & blue <= 8'haa & blue >= 8'h33;
 assign pink_detect = red[7] & green <= 8'hee & green >= 8'h66 & blue <= 8'he5 & blue >= 8'h88;
@@ -98,13 +100,8 @@ assign white_detect = red[7] & red[6] & green[7] & green[6] & blue[7] & blue[6];
 wire [23:0] rgb_high;
 assign grey = green[7:1] + red[7:2] + blue[7:2]; //Grey = green/2 + red/4 + blue/4
 // filter/detect by descending order/magnitude of RGB colour space
-assign rgb_high = white_detect ? {grey, grey, grey}
-				: blue_detect ? {8'h0, 8'h0, 8'hff} 
-				: pink_detect ? {8'hff, 8'haa, 8'hcc} 
-				: red_detect ? {8'hff, 8'h0, 8'h0} 
-				: green_detect ? {8'h0, 8'hff, 8'h0} 
-				: yellow_detect ? {8'hff, 8'hff, 8'h66} 
-				: {grey, grey, grey};
+assign rgb_high = filter ? ((white_detect&brightness_filter) ? {grey, grey, grey} : (blue_detect&brightness_filter) ? {8'h0, 8'h0, 8'hff} : (pink_detect&brightness_filter) ? {8'hff, 8'haa, 8'hcc} : (red_detect&brightness_filter) ? {8'hff, 8'h0, 8'h0} : (green_detect&brightness_filter) ? {8'h0, 8'hff, 8'h0} : (yellow_detect&brightness_filter) ? {8'hff, 8'hff, 8'h66} : {grey, grey, grey})
+						 : (white_detect ? {grey, grey, grey} : blue_detect ? {8'h0, 8'h0, 8'hff} : pink_detect ? {8'hff, 8'haa, 8'hcc} : red_detect ? {8'hff, 8'h0, 8'h0} : green_detect ? {8'h0, 8'hff, 8'h0} : yellow_detect ? {8'hff, 8'hff, 8'h66} : {grey, grey, grey});
 
 // Show bounding box
 wire [23:0] new_image;
@@ -148,36 +145,69 @@ reg [10:0] x_min_b, y_min_b, x_max_b, y_max_b;
 reg [10:0] x_min_y, y_min_y, x_max_y, y_max_y;
 reg [10:0] x_min_p, y_min_p, x_max_p, y_max_p;
 always @(posedge clk) begin
-	if (red_detect & in_valid) begin	// Update bounds when the pixel is red
-		if (x < x_min_r) x_min_r <= x;
-		if (x > x_max_r) x_max_r <= x;
-		if (y < y_min_r) y_min_r <= y;
-		y_max_r <= y;
+if (filter) begin
+		if (brightness_filter & red_detect & in_valid) begin	// Update bounds when the pixel is red
+			if (x < x_min_r) x_min_r <= x;
+			if (x > x_max_r) x_max_r <= x;
+			if (y < y_min_r) y_min_r <= y;
+			y_max_r <= y;
+		end
+		if (brightness_filter & green_detect & in_valid) begin	// green
+			if (x < x_min_g) x_min_g <= x;
+			if (x > x_max_g) x_max_g <= x;
+			if (y < y_min_g) y_min_g <= y;
+			y_max_g <= y;
+		end
+		if (brightness_filter & blue_detect & in_valid) begin	// blue
+			if (x < x_min_b) x_min_b <= x;
+			if (x > x_max_b) x_max_b <= x;
+			if (y < y_min_b) y_min_b <= y;
+			y_max_b <= y;
+		end
+		if (brightness_filter & yellow_detect & in_valid) begin	// yellow
+			if (x < x_min_y) x_min_y <= x;
+			if (x > x_max_y) x_max_y <= x;
+			if (y < y_min_y) y_min_y <= y;
+			y_max_y <= y;
+		end
+		if (brightness_filter & pink_detect & in_valid) begin	// pink
+			if (x < x_min_p) x_min_p <= x;
+			if (x > x_max_p) x_max_p <= x;
+			if (y < y_min_p) y_min_p <= y;
+			y_max_p <= y;
+		end
+	end else begin
+			if (red_detect & in_valid) begin	// Update bounds when the pixel is red
+			if (x < x_min_r) x_min_r <= x;
+			if (x > x_max_r) x_max_r <= x;
+			if (y < y_min_r) y_min_r <= y;
+			y_max_r <= y;
+		end
+		if (green_detect & in_valid) begin	// green
+			if (x < x_min_g) x_min_g <= x;
+			if (x > x_max_g) x_max_g <= x;
+			if (y < y_min_g) y_min_g <= y;
+			y_max_g <= y;
+		end
+		if (blue_detect & in_valid) begin	// blue
+			if (x < x_min_b) x_min_b <= x;
+			if (x > x_max_b) x_max_b <= x;
+			if (y < y_min_b) y_min_b <= y;
+			y_max_b <= y;
+		end
+		if (yellow_detect & in_valid) begin	// yellow
+			if (x < x_min_y) x_min_y <= x;
+			if (x > x_max_y) x_max_y <= x;
+			if (y < y_min_y) y_min_y <= y;
+			y_max_y <= y;
+		end
+		if (pink_detect & in_valid) begin	// pink
+			if (x < x_min_p) x_min_p <= x;
+			if (x > x_max_p) x_max_p <= x;
+			if (y < y_min_p) y_min_p <= y;
+			y_max_p <= y;
+		end
 	end
-	if (green_detect & in_valid) begin	// green
-		if (x < x_min_g) x_min_g <= x;
-		if (x > x_max_g) x_max_g <= x;
-		if (y < y_min_g) y_min_g <= y;
-		y_max_g <= y;
-	end
-	if (blue_detect & in_valid) begin	// blue
-		if (x < x_min_b) x_min_b <= x;
-		if (x > x_max_b) x_max_b <= x;
-		if (y < y_min_b) y_min_b <= y;
-		y_max_b <= y;
-	end
-	if (yellow_detect & in_valid) begin	// yellow
-		if (x < x_min_y) x_min_y <= x;
-		if (x > x_max_y) x_max_y <= x;
-		if (y < y_min_y) y_min_y <= y;
-		y_max_y <= y;
-	end
-	if (pink_detect & in_valid) begin	// pink
-		if (x < x_min_p) x_min_p <= x;
-		if (x > x_max_p) x_max_p <= x;
-		if (y < y_min_p) y_min_p <= y;
-		y_max_p <= y;
-	end 
 	if (sop & in_valid) begin	// Reset bounds on start of packet
 		x_min_r <= IMAGE_W-11'h1; x_max_r <= 0;
 		y_min_r <= IMAGE_H-11'h1; y_max_r <= 0; // red
@@ -240,7 +270,41 @@ always@(posedge clk) begin
 	// Cycle through message writer states once started
 	if (msg_state != 2'b00) msg_state <= msg_state + 2'b01;
 end
-	
+
+// Compute distance to balls
+wire [9:0] box_width_r, box_width_g, box_width_b, box_width_y, box_width_p; // max 640 < 1024 = 2^10
+wire [8:0] box_height_r, box_height_g, box_height_b, box_height_y, box_height_p;
+assign box_width_r = x_max_r - x_min_r;
+assign box_width_g = x_max_g - x_min_g;
+assign box_width_b = x_max_b - x_min_b;
+assign box_width_y = x_max_y - x_min_y;
+assign box_width_p = x_max_p - x_min_p;
+assign box_height_r = y_max_r - y_min_r;
+assign box_height_g = y_max_g - y_min_g;
+assign box_height_b = y_max_b - y_min_b;
+assign box_height_y = y_max_y - y_min_y;
+assign box_height_p = y_max_p - y_min_p;
+
+// Compute the perpendicular distance from the camera to the balls + distance from camera to the optical sensor displacement readings
+wire [7:0] dis_r, dis_g, dis_b, dis_y, dis_p; 	// 8 bits: max 256 cm away from rover's camera
+wire [9:0] focal_length; 						// all real life lengths in cm
+assign focal_length = 10'd800; 					// units: box width in pixels
+assign dis_r = 4 * focal_length / box_width_r + 24; 	// 4 cm ball diameter and 24 cm from camera to optical flow sensor
+assign dis_g = 4 * focal_length / box_width_g + 24;
+assign dis_b = 4 * focal_length / box_width_b + 24;
+assign dis_y = 4 * focal_length / box_width_y + 24;
+assign dis_p = 4 * focal_length / box_width_p + 24;
+
+// Compute angle from the centre of camera vision using perpendicular distance and number of pixels from the centre of camera (320,240)
+// Rather than computing the angle by arctan on verilog / hardware (not efficient use) -> let ESP32 do this
+// Find the number of pixels apart from the centre of vision to the balls
+wire signed [9:0] pixel_r, pixel_g, pixel_b, pixel_y, pixel_p;
+assign pixel_r = (x_min_r + box_width_r >> 2) - 320; // centre of ball pixel to the right of centre: +ve
+assign pixel_g = (x_min_r + box_width_g >> 2) - 320; // 					 to the  left of centre: -ve
+assign pixel_b = (x_min_r + box_width_b >> 2) - 320;
+assign pixel_y = (x_min_r + box_width_y >> 2) - 320;
+assign pixel_p = (x_min_r + box_width_p >> 2) - 320;
+
 // Generate output messages for CPU
 reg	[31:0]	msg_buf_in;
 reg msg_buf_wr;
@@ -257,15 +321,15 @@ always @(*) begin	// Write words to FIFO as state machine advances
 			msg_buf_wr = 1'b0;				// nothing written in buffer
 		end
 		2'b01: begin
-			msg_buf_in = `RED_BOX_MSG_ID;	// Message ID: "RBB" in hex 524242
+			msg_buf_in = {dis_r,dis_g,dis_b,dis_y};	// distance to the balls r,g,b,y
 			msg_buf_wr = 1'b1;
 		end
 		2'b10: begin
-			msg_buf_in = {5'b0, x_min_r, 5'b0, y_min_r};	// Top left coordinate
+			msg_buf_in = {dis_p,2'd0,pixel_r,2'd0,pixel_g};	// distance to the ball p, pixel distance to the centre of r, g
 			msg_buf_wr = 1'b1;
 		end
 		2'b11: begin
-			msg_buf_in = {5'b0, x_max_r, 5'b0, y_max_r}; // Bottom right coordinate
+			msg_buf_in = {pixel_b,1'd0,pixel_y,1'd0,pixel_p}; // pixel distance to the centre of b, y, p
 			msg_buf_wr = 1'b1;
 		end
 	endcase
