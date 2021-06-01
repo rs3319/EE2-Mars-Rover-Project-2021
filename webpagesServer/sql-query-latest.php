1edefdda24389bfbcace56c55a1ca19f7b973be8
Alden0012 <59955644+Alden0012@users.noreply.github.com>
2021-06-01 14:29:04 +0400

    can now get X,Y coords and speed from database

--- webpagesServer/sql-query-latest.php
+++ webpagesServer/sql-query-latest.php
@@ -19,7 +19,19 @@ if ($_SERVER["REQUEST_METHOD"] == "GET"){
         $row = $result->fetch_assoc();
         echo $row["BatteryLevel"];
         }
-        else if (test_input($_GET["database"]) == "commands"){
+        else if(test_input($_GET["database"]) == "position"){
+        $sql = "SELECT PositionX,PositionY from ESP32DriveReadings WHERE id=(SELECT max(id) FROM ESP32DriveReadings)";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        echo "X: ".$row["PositionX"].", Y: ".$row["PositionY"];    
+            
+        }else if(test_input($_GET["database"]) == "speed"){
+        $sql = "SELECT Speed from ESP32DriveReadings WHERE id=(SELECT max(id) FROM ESP32DriveReadings)";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+        echo $row["Speed"];    
+            
+        }else if (test_input($_GET["database"]) == "commands"){
         $sql = "SELECT id,command,param1,param2 from ESP32Commands WHERE command_timestamp=(SELECT min(command_timestamp) FROM ESP32Commands)";
         $result = $conn->query($sql);
         if($result->num_rows > 0){
