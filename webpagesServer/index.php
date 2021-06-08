a2a2836e5edb1d838cdd401558cd66573a94adfb
Alden0012 <59955644+Alden0012@users.noreply.github.com>
2021-06-08 16:19:25 +0400

    made it look a little nicer maybe

--- webpagesServer/index.php
+++ webpagesServer/index.php
@@ -18,18 +18,19 @@
     </head>
     <body>
     
-    <h1 class ="text-center" align="center" style="color:yellow;">ESP32 Mars Rover Dashboard</h1> <br>
+        <h1 class ="text-center" align="center" style="color:yellow;">ESP32 Mars Rover Dashboard</h1> <br>
 
         <section class="main">
         
-    <h2 class="text-center" align="center">Rover Position</h2>
+    <h2 class="text-center" align="center" id = "GraphText">Rover Position</h2>
     <style>
         .chart_box {
         display: inline-block;
         margin: 0 auto;
     }
     </style>
-    <div id="regions_div" class = "chart_box"  style="width: 600px; height: 350px;"></div>
+    <table align="center"><tr><td><div id="regions_div" class = "chart_box"  style="width: 600px; height: 350px;"></div></td>
+    <td><div id="energy_div" class = "chart_box"  style="width: 600px; height: 350px;"></div></td></tr></table>
     
     
     <script type="text/javascript">
@@ -37,7 +38,30 @@
         'packages':['corechart'],
         'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
       });
-      
+      function drawEnergyPlot() {
+        var jsonData = $.ajax({
+            url: "esp32-get-energy-array.php",
+            dataType:"json",
+            async: false,
+            success: function (data) {
+                var arr = [['id','BatteryLevel']];    // Define an array and assign columns for the chart.
+
+                // Loop through each data and populate the array.
+                $.each(data, function (index, value) {
+
+                    arr.push([new Date(Date.parse(value.reading_time)),parseInt(value.BatteryLevel)]);
+                });
+        var data = google.visualization.arrayToDataTable(arr);
+
+
+        var chart = new google.visualization.LineChart(document.getElementById('energy_div'));
+
+        chart.draw(data);
+            }
+        
+            
+        });
+      }
       
       function drawRegionsMap() {
         var jsonData = $.ajax({
@@ -61,44 +85,80 @@
             }
         });
       }
+       function drawTerrainMap() {
+        var jsonData = $.ajax({
+            url: "esp32-get-ping-pong.php",
+            dataType:"json",
+            async: false,
+            success: function (data) {
+                var arr = [['X','Y',{role: 'tooltip'},{role: 'style'}]];    // Define an array and assign columns for the chart.
+                // Loop through each data and populate the array.
+                $.each(data, function (index, value) {
+                    arr.push([parseInt(value.X),parseInt(value.Y),value.Color,"color: " + value.Color.toLowerCase()]);
+                    
+                });
+        var data = google.visualization.arrayToDataTable(arr);
+        var options = {vAxis: {viewWindow: {min: -100, max: 100}},hAxis: {viewWindow: {min: -100, max: 100}}, legend: {position: 'none'},explorer: {
+            keepInBounds: true,
+            maxZoomIn: 4.0
+          }
+                        };
+        var chart = new google.visualization.ScatterChart(document.getElementById('regions_div'));
+        chart.draw(data, options);
+            }
+        
+            
+        });
+      }
+      function MapSelector(){
+          if(mapMode){
+              drawRegionsMap();
+              document.getElementById("GraphText").innerHTML = "Rover Position";
+          }else{
+              drawTerrainMap();
+              document.getElementById("GraphText").innerHTML = "Terrain Map of Obstacles";
+          }
+      }
     </script>
     <script type="text/javascript" src="jQuery.js"></script>
         <script type="text/javascript">
-
+        var mapMode = false;
                 $(document).ready(function(){
                     // First load the chart once 
                     drawRegionsMap();
+                    drawEnergyPlot();
                     // Set interval to call the drawChart again
-                    setInterval(drawRegionsMap, 2000);
+                    setInterval(MapSelector(mapMode), 2000);
                     });
+                    
         </script>
        
             <section class="center" id="buttons">
                 
 
-                <div id="controls" class="align-items:center;" align="center">
+                <div id="controls" class="align-items:left;" align="left">
                     
-                  <table>
-                  <tr><td align="center"><button class="button button6" id="get-still">Image</button></td><td align="center"><button id="toggle-stream">Start</button></td><td></td></tr>
-                  <tr><td></td><td align="center"><button class="button button2" id="forward" >FORWARD</button></td><td></td><td align="left"><p align = "left">
-          <i class="fas fa-lightbulb" style="color:#059e8a;"></i> 
-          <span class="dht-labels">Energy</span> 
-          <span id="energyvalue">%ENERGY%</span>
-        </p></td></tr>
-                  <tr><td align="center"><button class="button button2" id="turnleft" >LEFT</button></td><td align="center"></td><td align="center"><button class="button button2" id="turnright" >RIGHT</button></td><td align="left">     
+                  <table align="left">
+                  <tr><td align="left">     
         <p align = "left">
           <i class="fas fa-map-marker-alt" style="color:#059e8a;"></i> 
           <span class="dht-labels">Position</span> 
           <span id="positionvalue">%POSITION%</span>
-        </p>  
-        </td></tr>
-                  <tr><td></td><td align="center"><button class="button button2" id="backward" >REVERSE</button></td><td></td><td align="left"><p align = "left">
+        </p><p align = "left">
           <i class="fas fa-tachometer-alt" style="color:#059e8a;"></i> 
           <span class="dht-labels">Speed</span> 
           <span id="speedvalue">%SPEED%</span>
-        </p> </td></tr>
-                  <tr><td align="center"><button onclick="location.href='http://esp32-mars-rover.000webhostapp.com/sql_energy_chart.php'" type="button"> View Energy Status</button></td><td></td><td align="center"><button onclick="location.href='http://esp32-mars-rover.000webhostapp.com/sql-terrain-map.php'" type="button">
-         View Terrain Map</button></td><td></td><td></td><td></td><td align="right"> <p align ="right">
+        </p> 
+        </td><td align="center"><button id="toggle-stream">Start</button><button onclick="Toggle()" type="button">
+         Toggle Map</button></td><td></td><td></td><td></td><td align="left"><p align = "left">
+          <i class="fas fa-lightbulb" style="color:#059e8a;"></i> 
+          <span class="dht-labels">Energy</span> 
+          <span id="energyvalue">%ENERGY%</span>
+        </p></td><td></td></tr>
+                  <tr><td></td><td align="center"><button class="button button2" id="forward" >Forward by 10</button></td><td></td></tr>
+                  <tr><td align="center"><button class="button button2" id="turnleft" >Turn Left by 90</button></td><td align="center"></td><td align="center"><button class="button button2" id="turnright" >Turn Right by 90</button></td></tr>
+                  <tr><td></td><td align="center"><button class="button button2" id="backward" >Backward by 10</button></td><td></td></tr>
+                  <tr><td></td><td align="right"> <p align ="right">
                     Move to coordinate:
                     <br>
                     <br>
@@ -123,9 +183,11 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 
         <script>
-
+        
           $( "#spinnerX" ).spinner();
+          $("#spinnerX").width(30);
           $( "#spinnerY" ).spinner();
+          $("#spinnerY").width(30);
           document.getElementById("forward").onclick = DriveForward;
           document.getElementById("turnleft").onclick = DriveLeft;
           document.getElementById("turnright").onclick = DriveRight;
@@ -136,17 +198,22 @@
             $.get("sql-query-latest.php",{api_key: "EXAMPLEKEY2000",database : "position"},function(data,status){document.getElementById("positionvalue").innerHTML = data})
             $.get("sql-query-latest.php",{api_key: "EXAMPLEKEY2000",database : "speed"},function(data,status){document.getElementById("speedvalue").innerHTML = data})
             
+            MapSelector(mapMode);
+            drawEnergyPlot();
           },5000)
-
+          function Toggle(){
+              mapMode = !mapMode;
+              MapSelector(mapMode);
+          }
           function DriveLeft(){
-            $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "tl", param1: 20, param2: 0});
+            $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "tl", param1: 90, param2: 0});
           }
 
           function DriveForward(){
             $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000", name : "fw", param1: 10, param2: 0});
           }
           function DriveRight(){
-            $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "tr", param1: 20, param2: 0})
+            $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "tr", param1: 90, param2: 0})
           }
           function DriveReverse(){
             $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "rv", param1: 10, param2: 0})
@@ -155,7 +222,7 @@
  
             $.post("esp-post-command.php",{api_key: "EXAMPLEKEY2000",name : "mv", param1: document.getElementById("spinnerX").value, param2: document.getElementById("spinnerY").value }) 
           }
-          
+
         </script>
     </body>
 </html>
