c39dc2a513b1d5dffe3bc9a0e58047d57f244b0f
Alden0012 <59955644+Alden0012@users.noreply.github.com>
2021-06-09 16:05:37 +0400

    simpler drive code also uses hardwareSerial.h

--- drivetest.ino
+++ drivetest.ino
@@ -1,5 +1,5 @@
-int PosX = 0;
-int PosY = 0;
+#include <HardwareSerial.h>
+
 void setup() {
   // put your setup code here, to run once:
 Serial1.begin(9600);
@@ -8,14 +8,12 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
-PosX += 10;
-PosY += 10;
 Serial.println("Reading");
 if(Serial1.available()){
   String Status = Serial1.readStringUntil('\n');
-  Serial.println(Status);
+  Serial.println("Read: " + Status);
 }
 Serial.println("Writing");
-String ToWrite = (String)PosX +","+ (String)PosY + ",0,0";
+String ToWrite = "Hello";
 Serial1.println(ToWrite);
 }
