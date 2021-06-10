a68d8c49203f379706d34fbfc1c8a4cd7ac1fd60
Alden0012 <59955644+Alden0012@users.noreply.github.com>
2021-06-10 13:54:52 +0400

    more testing for control-drive

--- drivetest.ino
+++ drivetest.ino
@@ -1,19 +1,24 @@
 #include <HardwareSerial.h>
-
+#include <string>
+// DRIVE
 void setup() {
   // put your setup code here, to run once:
-Serial1.begin(9600);
 Serial.begin(9600);
+Serial1.begin(9600,SERIAL_8N1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 Serial.println("Reading");
 if(Serial1.available()){
-  String Status = Serial1.readStringUntil('\n');
-  Serial.println("Read: " + Status);
+  //String Status = Serial1.readStringUntil('\n');
+  std::string Status = Serial1.readStringUntil('\n').c_str();
+  //std::string Status = Serial1.readStringUntil(';').c_str();
+  Serial.println(Status.c_str());
 }
 Serial.println("Writing");
-String ToWrite = "Hello";
+String ToWrite = "Hello from Drive";
 Serial1.println(ToWrite);
+delay(1000);
 }
